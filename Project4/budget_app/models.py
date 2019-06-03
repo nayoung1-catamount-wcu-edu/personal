1ef4b5617eb53260704aa07c35fe642104a10e06
nayoung1-catamount-wcu-edu <nathanayoung@msn.com>
2019-06-03 19:58:23 -0400

    Uploading CIS 240 Project 4

---
+++ Project4/budget_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Transaction(models.Model):
+  text = models.CharField(max_length=200)
+  date_added = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.text
+
+class Entry(models.Model):
+  transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+  merchant = models.CharField(max_length=200)
+  description = models.TextField()
+  transaction_amount = models.FloatField(max_length=10)
+  date_added = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    verbose_name_plural = 'entries'
+
+  def __str__(self):
+    return self.merchant
