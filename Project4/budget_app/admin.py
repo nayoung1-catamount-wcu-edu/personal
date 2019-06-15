0fc197321834137f084ff55f16916d10b748272b
nayoung1-catamount-wcu-edu <nathanayoung@msn.com>
2019-06-14 23:28:23 -0400

    deleted project 4

--- Project4/budget_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here
-from budget_app.models import Transaction, Entry
-
-admin.site.register(Transaction)
-admin.site.register(Entry)
--- Project4/budget_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BudgetAppConfig(AppConfig):
-    name = 'budget_app'
