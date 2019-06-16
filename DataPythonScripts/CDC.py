fb6250fd12fb07626980b0fe9bb35a2ad261cc40
Nathan Young <46688736+grumpyphotographer@users.noreply.github.com>
2019-06-16 10:33:04 -0400

    Delete CDC.py

--- DataPythonScripts/CDC.py
+++
@@ -1,2 +0,0 @@
-## This project takes a look at the CDC's "500 Cities: Local Data for Better Health, 2017 release" located here: https://catalog.data.gov/dataset/500-cities-local-data-for-better-health-2017-release ##
-
