fd6ef58f372b313fdda6abac51cd76a642ceac9f
nayoung1-catamount-wcu-edu <nathanayoung@msn.com>
2019-06-16 22:51:20 -0400

    updated kinematics

--- BasicPythonScripts/Kinematics.py
+++ BasicPythonScripts/Kinematics.py
@@ -1,102 +1,97 @@
-print('Values are as follows: \nx = displacement: xi, xf','\nv = velocity: vi, vf','\nt = time','\na = acceleration')
+print('Values are as follows: \nd = displacement: d/x/yi, d/x/yf','\nv = velocity: vi, vf','\nt = time','\na = acceleration')
 
-equation = (input('\nWhat value are you trying to find? '))
+def runProgram():
+  equation = (input('\nWhat value are you trying to find? '))
 
-if equation == 'xi':
-  print('Finding xi')
-  value = 0
-  while value == 0:
-    try:
-      xf = int(input('Enter a value for final displacement: '))
-      vi = int(input('Enter value for initial velocity: '))
-      vf = int(input('Enter value for final velocity: '))
-      a = int(input('Enter value for acceleration: '))
-      t = int(input('Enter a value for time: '))
-      xi = (xf-(vi*t)-(0.5*(-a)*(t**2)))
-      xi2 = (xf-((vf-vi)/2)*t)
-      print(xi == xi2, xi)
-    except:
-      print('Please enter a number. If you are solving for this variable, press "1".')
-
-elif equation == 'xf':
-  print('Finding xf')
-  value = 0
-  while value == 0:
-    try:
-      xi = int(input('Solve for initial displacement: '))
-      vi = int(input('Enter value for initial velocity: '))
-      vf = int(input('Enter value for final velocity: '))
-      t = int(input('Enter a value for time: '))
-      a = int(input('Enter value for acceleration: '))
-      xf = (vi*t)+(0.5*(-a)*(t**2))+xi
-      xf2 = (((vf+vi)/2)*t)+xi
-      xf == xf2
-      print(xf == xf2)
-    except:
-      print('Please enter a number. If you are solving for this variable, press "1".')
-
-elif equation == 'a':
-  print('Finding a')
-  value = 0
-  while value == 0:
-    try:
-      vi = int(input('Enter value for initial velocity: '))
-      vf = int(input('Enter value for final velocity: '))
-      t = int(input('Enter a value for time: '))
-      a = ((vf-vi)/t)
-      print(a)
-    except:
-      print('Please enter a number. If you are solving for this variable, press "1".')
-  
-
-elif equation == 't':
-  print('Finding t')
-  value = 0
-  while value == 0:
-    try:
-      vi = int(input('Enter value for initial velocity: '))
-      vf = int(input('Enter value for final velocity: '))
-      a = int(input('Enter value for acceleration: '))
-      t = ((vf-vi)/(-a))
-      print(t)
-    except:
-      print('Please enter a number. If you are solving for this varible, press "1".')
-  
-
-elif equation == 'vf':
-  print('Finding vf')
-elif equation == 'vi':
-  print('Finding vi')
+  if equation == 'xi' or equation == 'yi' or equation == 'di':
+    print('Finding Initial Displacement')
+    value = 0
+    while value == 0:
+      try:
+        df = int(input('Enter a value for final displacement: '))
+        vi = int(input('Enter value for initial velocity: '))
+        vf = int(input('Enter value for final velocity: '))
+        a = int(input('Enter value for acceleration: '))
+        t = int(input('Enter a value for time: '))
+        di = (xf-(vi*t)-(0.5*(-a)*(t**2)))
+        di2 = (xf-((vf-vi)/2)*t)
+        print(di == di2, di)
+      except:
+        break
 
 
+  elif equation == 'xf' or equation == 'yf' or equation == 'df':
+    print('Finding Final Displacement')
+    value = 0
+    while value == 0:
+      try:
+        di = int(input('Solve for initial displacement: '))
+        vi = int(input('Enter value for initial velocity: '))
+        vf = int(input('Enter value for final velocity: '))
+        t = int(input('Enter a value for time: '))
+        a = int(input('Enter value for acceleration: '))
+        df = (vi*t)+(0.5*(-a)*(t**2))+di
+        df2 = (((vf+vi)/2)*t)+di
+        df == df2
+        print(df == df2)
+      except:
+        break
 
-'''value = 0
-while value == 0:
- try:
-  vf = int(input('Enter value for final velocity: '))
-  if vf >= 0 or vf < 0:
-   value = 1
-   break
- except:
-  print('Please enter a number.')
-  continue
+
+  elif equation == 'a':
+    print('Finding Acceleration')
+    value = 0
+    while value == 0:
+      try:
+        vi = int(input('Enter value for initial velocity: '))
+        vf = int(input('Enter value for final velocity: '))
+        t = int(input('Enter a value for time: '))
+        a = ((vf-vi)/t)
+        print(a)
+      except:
+        break
+    
+
+  elif equation == 't':
+    print('Finding Time')
+    value = 0
+    while value == 0:
+      try:
+        vi = int(input('Enter value for initial velocity: '))
+        vf = int(input('Enter value for final velocity: '))
+        a = int(input('Enter value for acceleration: '))
+        t = ((vf-vi)/(-a))
+        print(t)
+      except:
+        break
+    
+
+  elif equation == 'vf':
+    print('Finding Final Velocity')
+    value = 0
+    while value == 0:
+      try:
+        vi = int(input('Enter value for initial velocity: '))
+        a = int(input('Enter value for acceleration: '))
+        t = int(input('Enter a value for time: '))
+        vf = vi + a*t
+        print(vf)
+      except:
+        break
 
 
+  elif equation == 'vi':
+    print('Finding Initial Velocity')
+    value = 0
+    while value == 0:
+      try:
+        vf = int(input('Enter value for final velocity: '))
+        a = int(input('Enter value for acceleration: '))
+        t = int(input('Enter a value for time: '))
+        vf = vi + a*t
+        print(vi)
+      except:
+        break
 
-value = 0
-while value == 0:
- try:
-  t = int(input('Enter a value for time: '))
-  if t >= 0 or t < 0:
-   value = 1
-   break
- except:
-  print('Please enter a number.')
-  continue
-
-#x = (vi*t) + (0.5*a*(t**2))
-
-#(xf-xi) == ((0.5*(vf + vi))*t)
-#(vf**2) == ((vi**2) + (2*a*(xf-xi)))
-#vf == vi + (a*t)
-#(xf-xi) == (vf*t - (0.5(a)(t**2)))'''
+while True:
+  runProgram()
