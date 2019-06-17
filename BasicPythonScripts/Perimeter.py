89cd40cedffacc80f42f2095fab0487f101d34f7
nayoung1-catamount-wcu-edu <nathanayoung@msn.com>
2019-06-17 10:52:38 -0400

    editing perimeter

--- BasicPythonScripts/Perimeter.py
+++ BasicPythonScripts/Perimeter.py
@@ -1,6 +1,5 @@
-length = int(input('Enter length: ')
-width = int(input('Enter width: ')
+length = int(input('Enter length: '))
+width = int(input('Enter width: '))
 
-perimeter = 2*(length+width)
-
-print(perimeter)
+perimeter = 2*(length + width)
+print('Perimeter is: ',perimeter)
