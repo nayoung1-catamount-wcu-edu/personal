a1765d68126181f6bd13d9f13de5287141a9cb93
nayoung1-catamount-wcu-edu <nathanayoung@msn.com>
2019-07-02 19:03:24 -0400

    uploading payCalculator.py

---
+++ BasicPythonScripts/payCalculator.py
@@ -0,0 +1,12 @@
+hourly_pay = float(input('Enter hourly pay: '))
+hours_worked = float(input('Enter hours worked: '))
+day_pay = hourly_pay * hours_worked
+
+total_sales = float(input('Enter sales: '))
+commission = float(input('Enter commission: '))
+day_sales = total_sales * commission
+
+if day_pay > day_sales:
+ print('Today you earned hourly pay equal to:',day_pay)
+else:
+ print('Today you earned commission equal to:',day_sales)
