afa21738190dad0aac51fb8c50818980cfeb9925
nayoung1-catamount-wcu-edu <nathanayoung@msn.com>
2019-07-22 18:54:41 -0400

    editing bible verse generator

--- BasicPythonScripts/RandomBibleVerseGenerator.py
+++ BasicPythonScripts/RandomBibleVerseGenerator.py
@@ -3,11 +3,9 @@ import random
 def runProgram():
  input('Press Enter to run generator:')
  random_number = random.sample(range(1001001,1050026), 1)
- for number in random_number:
-  print(number)
+ print(random_number)
 
 while True:
  runProgram()
-
 
 ## id format = bb/ccc/vvv
