f5e0c00b71b0862f982f7e3fbe0ba393cff071e3
nayoung1-catamount-wcu-edu <nathanayoung@msn.com>
2019-07-30 20:28:31 -0400

    editing RandomBibleVerseGenerator.py

--- BasicPythonScripts/RandomBibleVerseGenerator.py
+++ BasicPythonScripts/RandomBibleVerseGenerator.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import random
 
+# Begin app
 input('Press a key to begin...')
 
+# Load dataframe
 df = pd.read_excel('/Users/owner/Onedrive/Desktop/personal/Data/finished_bible.xlsx')
 
-#random_number = 1001001
+# Get a random verse
 n = random.randint(0,31102)
 n2 = n+1
 
+# Print random verse
 print(df[n:n2])
