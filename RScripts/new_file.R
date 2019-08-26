648d1699de9031f1b296d9b126f2d61ada3fe702
grumpyphotographer <nathanayoung@msn.com>
2019-08-25 20:27:30 -0400

    graphing stocks in R

--- RScripts/new_file.R
+++ RScripts/new_file.R
@@ -1,6 +1,19 @@
 # Set working directory
 setwd('C:/Users/natha/OneDrive/Desktop/personal/RScripts')
 
-load('C:/Users/natha/OneDrive/Desktop/personal/Data/closing_stocks.csv')
+if (!require("quantmod")) {
+  install.packages("quantmod")
+  library(quantmod)
+}
 
-## graph stock data
+start <- as.Date("2019-01-01")
+end <- as.Date("2019-12-31")
+
+getSymbols(c("MSFT","AAPL","GOOG"), src = 'yahoo', from = start, to = end)
+
+stocks <- as.xts(data.frame(AAPL = AAPL[, "AAPL.Close"], MSFT = MSFT[, "MSFT.Close"], 
+                            GOOG = GOOG[, "GOOG.Close"]))
+head(stocks)
+
+plot(as.zoo(stocks), screens = 1, lty = 1:3, xlab = "Date", ylab = "Price")
+legend("right", c("AAPL", "MSFT", "GOOG"), lty = 1:3, cex = 0.5)
