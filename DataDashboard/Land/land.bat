4bf40b8742e0040d418eab0b0a80a530b698fa3d
nathayoung <nathanayoung@msn.com>
2019-12-16 13:48:14 -0500

    Land Update 12/2019

--- DataDashboard/Land/land.bat
+++ DataDashboard/Land/land.bat
@@ -1,10 +1,15 @@
+git config --global user.name "nathayoung"
+git status
+
 python -W ignore Scripts\County_MedianListingPrice_AllHomes.py
 python -W ignore Scripts\County_MedianListingPricePerSqft_AllHomes.py
 python -W ignore Scripts\County_MedianValuePerSqft_AllHomes.py
 python -W ignore Scripts\County_Zhvi_AllHomes.py
-python Scripts\GeoFRED_All_Transactions_House_Price_Index.py
-python Scripts\GeoFRED_Homeownership_Rate_by_County.py
-python Scripts\GeoFRED_New_Private_Housing_Structures.py
-python Scripts\Land_Measures.py
+python -W ignore Scripts\GeoFRED_All_Transactions_House_Price_Index.py
+python -W ignore Scripts\GeoFRED_Homeownership_Rate_by_County.py
+python -W ignore Scripts\GeoFRED_New_Private_Housing_Structures.py
+
+git add -A
+git commit -m "Land Update 12/2019"
 
 pause
