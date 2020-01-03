4f5241526a74a0a4867432db6c995387baea2be9
nathayoung <nathanayoung@msn.com>
2020-01-03 14:21:12 -0500

    Land Update Fri 01/03/2020

--- DataDashboard/update.bat
+++ DataDashboard/update.bat
@@ -1,25 +1,30 @@
-git config --global user.name "NCDataDashboard"
+git config --global user.name "nathayoung"
 
 cd Land
 start land.bat
 cd..
 
+pause
 cd Labor
 start labor.bat
 cd..
 
+pause
 cd Earnings
 start earnings.bat
 cd..
 
+pause
 REM cd Demographics
 REM start Demographics.bat
 REM cd..
 
+pause
 REM cd Health
 REM start health.bat
 REM cd..
 
+pause
 REM cd Natural Products
 REM start natproducts.bat
 REM cd..
