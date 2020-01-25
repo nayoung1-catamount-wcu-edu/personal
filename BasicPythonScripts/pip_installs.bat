fbf2b510b763a10cfe70a0b32cdae30e0a039d20
nathayoung <nathanayoung@msn.com>
2020-01-24 19:07:10 -0500

    uploading pip_installs

---
+++ BasicPythonScripts/pip_installs.bat
@@ -0,0 +1,8 @@
+python -m pip install --upgrade pip --user
+pip install jupyter
+pip install jupyter lab
+pip install pandas
+pip install tensorflow
+pip install ZipFile
+pip install Requests
+pip install watermark
