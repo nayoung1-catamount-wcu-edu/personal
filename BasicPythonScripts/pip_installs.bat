9b0b97dae6f6a0c971b4dee836ea822072010a11
nathayoung <nathanayoung@msn.com>
2020-01-24 19:32:50 -0500

    Land Update Fri 01/24/2020

--- BasicPythonScripts/pip_installs.bat
+++ BasicPythonScripts/pip_installs.bat
@@ -1,10 +1,10 @@
 python -m pip install --upgrade pip --user
-pip install jupyter
-pip install jupyter lab
-pip install pandas
-pip install tensorflow
-pip install ZipFile
-pip install Requests
-pip install watermark
-pip install xlrd
+pip install jupyter --upgrade
+pip install jupyter lab --upgrade
+pip install pandas --upgrade
+pip install tensorflow --upgrade
+pip install ZipFile --upgrade
+pip install Requests --upgrade
+pip install watermark --upgrade
+pip install xlrd --upgrade
 
--- DataDashboard/update.bat
+++ DataDashboard/update.bat
@@ -1,3 +1,13 @@
+python -m pip install --upgrade pip --user
+pip install jupyter --upgrade
+pip install jupyter lab --upgrade
+pip install pandas --upgrade
+pip install tensorflow --upgrade
+pip install ZipFile --upgrade
+pip install Requests --upgrade
+pip install watermark --upgrade
+pip install xlrd --upgrade
+
 git config --global user.name "nathayoung"
 
 cd Land
