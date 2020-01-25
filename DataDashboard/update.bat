6390c33caab200e600ce1e19cd5c28751126dad0
nathayoung <nathanayoung@msn.com>
2020-01-24 19:36:03 -0500

    creating testing batch script

--- DataDashboard/update.bat
+++ DataDashboard/update.bat
@@ -1,14 +1,4 @@
-python -m pip install --upgrade pip --user
-pip install jupyter --upgrade
-pip install jupyter lab --upgrade
-pip install pandas --upgrade
-pip install tensorflow --upgrade
-pip install ZipFile --upgrade
-pip install Requests --upgrade
-pip install watermark --upgrade
-pip install xlrd --upgrade
-
-git config --global user.name "nathayoung"
+git config --global user.name "NCDataDashboard"
 
 cd Land
 call land.bat
