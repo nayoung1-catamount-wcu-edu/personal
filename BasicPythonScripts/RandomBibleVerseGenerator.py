69d44693cf3acad156bbc5a82e99c70d7526f3fc
nathayoung <nathanayoung@msn.com>
2020-06-05 12:52:12 -0400

    updates

--- BasicPythonScripts/RandomBibleVerseGenerator.py
+++ BasicPythonScripts/RandomBibleVerseGenerator.py
@@ -3,53 +3,57 @@
 
 # # Imports
 
-# In[54]:
+# In[1]:
 
 
 import pandas as pd
+import random 
 
 
 # # Bible Verse Data
 
 # ### Load and view file in dataframe
 
-# In[55]:
+# In[2]:
 
 
-df = pd.read_excel("/Users/owner/Onedrive/Desktop/personal/Data/bible_kjv.xlsx")
+df = pd.read_excel('/Users/natha/Onedrive/Desktop/GitHub/personal/Data/bible_kjv.xlsx')
 
 df.head(2)
 
 
 # ### View and edit columns
 
-# In[56]:
+# In[3]:
 
 
 df.columns
 
 
-# In[57]:
+# In[4]:
 
 
-rename_columns = {"b": "book", "c": "chapter", "v": "verse", "t": "text"}
+rename_columns = {'b':'book',
+                 'c':'chapter',
+                 'v':'verse',
+                 't':'text'}
 
-df.rename(columns=rename_columns, inplace=True)
+df.rename(columns = rename_columns, inplace=True)
 df.head(2)
 
 
 # ### View and edit datatypes
 
-# In[58]:
+# In[5]:
 
 
 df.dtypes
 
 
-# In[59]:
+# In[6]:
 
 
-df["book"] = pd.to_numeric(df["book"]).astype(object)
+df['book']=pd.to_numeric(df['book']).astype(object)
 df.dtypes
 
 
@@ -57,17 +61,17 @@ df.dtypes
 
 # ### Load and view file in dataframe
 
-# In[60]:
+# In[7]:
 
 
-df_books = pd.read_excel("/Users/owner/Onedrive/Desktop/personal/Data/bible_books.xlsx")
+df_books = pd.read_excel('/Users/natha/Onedrive/Desktop/GitHub/personal/Data/bible_books.xlsx')
 
 df_books.head(2)
 
 
 # ### View and edit columns
 
-# In[61]:
+# In[8]:
 
 
 df_books.columns
@@ -75,7 +79,7 @@ df_books.columns
 
 # ### View and edit datatypes
 
-# In[62]:
+# In[9]:
 
 
 df_books.dtypes
@@ -85,28 +89,62 @@ df_books.dtypes
 
 # ### Left-joining Bible Verse Data with Books Data to create complete Bible database
 
-# In[63]:
+# In[10]:
 
 
-df_bible = pd.merge(df_books, df, on="book", how="left")
+df_bible = pd.merge(df_books, df, on = 'book', how = 'left')
 df_bible.head()
 
 
 # ### Editing columns in new dataframe
 
-# In[64]:
+# In[11]:
 
 
-new_column_order = ["id", "book", "book_name", "chapter", "verse", "text"]
+new_column_order = ['id','book','book_name','chapter','verse','text']
 
 df_bible = df_bible[new_column_order]
 
-print("Number of rows: ", df_bible.shape[0])
-print("Number of columns: ", df_bible.shape[1])
+print('Number of rows: ',df_bible.shape[0])
+print('Number of columns: ',df_bible.shape[1])
 df_bible.head()
 
 
-# In[65]:
+# In[12]:
 
 
-df_bible.to_csv("/Users/owner/Onedrive/Desktop/personal/Data/finished_bible.csv")
+#df_bible = df_bible.set_index('id')
+
+df_bible.to_csv('/Users/natha/Onedrive/Desktop/GitHub/personal/Data/finished_bible.csv')
+
+
+# In[13]:
+
+
+df_bible.tail()
+
+
+# In[14]:
+
+
+range_low = 0
+
+
+# In[15]:
+
+
+range_high = 31102
+
+
+# In[16]:
+
+
+amount_of_variables = int(input('How many verses do you want? '))
+
+
+# In[17]:
+
+
+random_verse = random.sample(range(range_low, range_high), amount_of_variables)
+print(df_bible.book_name.loc[random_verse].to_string(), df_bible.chapter.loc[random_verse].to_string(), df_bible.verse.loc[random_verse].to_string(), df_bible.text.loc[random_verse].to_string())
+
--- BasicPythonScripts/Stocks.py
+++ BasicPythonScripts/Stocks.py
@@ -4,24 +4,10 @@
 # In[1]:
 
 
-import matplotlib.pyplot as plt
 import pandas as pd
 
 import pandas_datareader.data as web
 import datetime as dt
-
-
-# In[3]:
-
-
-import plotly.plotly as py
-import plotly.graph_objs as go
-import plotly
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-import cufflinks as cf
-
-cf.set_config_file(offline=True)
-
 
 # In[2]:
 
@@ -30,7 +16,7 @@ cf.set_config_file(offline=True)
 start = dt.datetime(2010, 1, 1)
 
 # End of Stock series:
-end = dt.datetime(2019, 12, 31)
+end = dt.datetime(2020, 12, 31)
 
 
 # In[4]:
@@ -48,22 +34,7 @@ df_closing = df["Close"]
 df_closing.head()
 
 
-# In[6]:
-
-
-df_closing.plot()
-
-
-# In[7]:
-
-
-df_closing.iplot(
-    kind="line",
-    title="Apple, IBM, Netflix, Microsoft, and Oracle Close: 01/2010-11/2019",
-)
-
-
 # In[12]:
 
 
-df_closing.to_csv("C:/Users/natha/OneDrive/Desktop/personal/Data/closing_stocks.csv")
+df_closing.to_csv("C:/Users/natha/OneDrive/Desktop/GitHub/personal/Data/closing_stocks.csv")
