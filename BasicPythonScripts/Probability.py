1706826890f094281a378173b0382d3e5e96ada0
nathayoung <nathanayoung@msn.com>
2020-06-11 00:38:08 -0400

    updates

--- BasicPythonScripts/Probability.py
+++ BasicPythonScripts/Probability.py
@@ -1,26 +1,41 @@
 import statistics
-import random
 
-try:
-    # Random Variable Mean
-        # x_bar = sample
-        # mu = population
+# Random Variable Mean
+    # x_bar = sample
+    # mu = population
 
-    def random_mean():
-        x = [int(x) for x in input('Enter all values for x, separated by spaces: ').split()]
+def random_mean():
+    x = [float(x) for x in input('Enter all values for x, separated by spaces: ').split()]
 
-        #Calculate Mean
-        mean = statistics.mean(x)
-        print('\nMean of', x, 'is:', round(mean, 4))  
+    #Calculate Mean
+    mean = statistics.mean(x)
+    print('\nMean of', x, 'is:', round(mean, 4))  
 
-        #Calculate Std Dev
-        std_dev = statistics.stdev(x)
-        print('\nStandard Deviation of', x, 'is:', round(std_dev, 4))
+    #Calculate Std Dev
+    std_dev = statistics.stdev(x)
+    print('\nStandard Deviation of', x, 'is:', round(std_dev, 4))
 
     while True:
         random_mean()
-        exit()
 
+
+# Probablility Distribution
+    # Variance
+        # sigma^2 = sum((x^2)*p(x)-mu^2)
+    # Standard Variation
+        # sigma = sqrt(variance)
+    # Mean
+        # mu = sum(x*p(x))
+
+def probability_distribution():
+
+    while True:
+        probability_distribution()
+
+try:
+    random_mean()
+    probability_distribution()
+    
 except statistics.StatisticsError:
     print('\nPlease enter at least 2 values.\n')
     random_mean()
