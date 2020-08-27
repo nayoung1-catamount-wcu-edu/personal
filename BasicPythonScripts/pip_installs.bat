34f461e8cb1bcab9ec65e3e59a37fd7811f9e155
nathayoung <nathanayoung@msn.com>
2020-08-26 22:20:19 -0400

    updates

--- BasicPythonScripts/pip_installs.bat
+++ BasicPythonScripts/pip_installs.bat
@@ -23,3 +23,4 @@ python -m pip install pandas_datareader --upgrade
 python -m pip install chart_studio --upgrade
 python -m pip install pandas_bokeh --upgrade
 py -m pip install pylint --upgrade
+py -m pip install spellchecker --upgrade
