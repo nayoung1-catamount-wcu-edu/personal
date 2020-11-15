5e445737c5d2c67486ef1e4e03b6f34c2a92efd9
nathayoung <nathanayoung@msn.com>
2020-11-15 00:14:22 -0500

    updates

---
+++ BasicRScripts/COVID/server.R
@@ -0,0 +1,64 @@
+library(reshape2)
+library(ggplot2)
+library(tidyverse)
+library(ggdark)
+
+# Define server logic
+server <- function(input, output, session) {
+  # read data
+  covid <-
+    read.csv(file.path("Data/covid-19-data", "us-counties.csv"))
+  
+  # melt data
+  covid_melt <-
+    melt(covid, id = c("date", "county", "state", "fips"))
+  
+  # order county values ascending
+  covid_melt <- covid_melt[order(covid_melt$county), ]
+  
+  # order state values ascending
+  covid_melt <- covid_melt[order(covid_melt$state), ]
+  
+  # convert date column to date dtype
+  covid_melt$date <- as.Date(covid_melt$date, format = "%Y-%m-%d")
+  
+  # output state_selector
+  output$state_selector <- renderUI({
+    selectInput(
+      inputId = "state",
+      label = "Choose a State/Territory:",
+      choices = unique(covid_melt$state),
+      selected = ""
+    )
+  })
+  
+  # output county_selector
+  output$county_selector <- renderUI({
+    available <- covid_melt[covid_melt$state == input$state, "county"]
+    
+    selectInput(
+      inputId = "county",
+      label = "Choose a County/Municipality:",
+      choices = unique(available),
+      selected = unique(available[1])
+    )
+  })
+  
+  # output range of dates
+  output$date_selector <- renderUI({
+    dateRangeInput(inputId = "dates",
+                   label = "Select a Range of Dates",
+                   start = "2020-11-01")
+  })
+  
+  # plot cases and deaths by selected state and county in selected state
+  output$covid_ggplot <- renderPlot({
+    county_filter <- filter(covid_melt, county == input$county)
+    ggplot(data = county_filter, aes(x = date,
+                                     y = value,
+                                     fill = variable)) +
+      geom_bar(stat = "identity") +
+      scale_x_date(limits = c(input$dates[1], input$dates[2])) +
+      dark_theme_minimal()
+  })
+}
---
+++ BasicRScripts/COVID/ui.R
@@ -0,0 +1,37 @@
+library(shinythemes)
+library(tidyverse)
+
+# read data
+covid <-
+  read.csv(file.path("Data/covid-19-data", "us-counties.csv"))
+
+
+
+# convert date column to date dtype
+covid$date <- as.Date(covid$date, format = "%Y-%m-%d")
+
+# build ui
+fluidPage(
+  # set theme
+  theme = shinytheme(theme = "cyborg"),
+  
+  # set title
+  titlePanel(title = "COVID Case and Deaths"),
+  
+  # create sidebar
+  sidebarLayout(
+    sidebarPanel(
+      # user input state value
+      htmlOutput("state_selector"),
+      
+      # user input county value eventually based on state value
+      htmlOutput("county_selector"),
+      
+      # user input date range
+      htmlOutput("date_selector")
+    ),
+    # print plot in main panel
+    mainPanel(plotOutput(outputId = "covid_ggplot"),),
+    
+  )
+)
