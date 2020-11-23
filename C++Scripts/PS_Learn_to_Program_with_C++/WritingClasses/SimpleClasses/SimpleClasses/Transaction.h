b546ef968ad44038dde0ce48294dad80082dc62f
nathayoung <nathanayoung@msn.com>
2020-11-23 00:17:15 -0500

    updates

---
+++ C++Scripts/PS_Learn_to_Program_with_C++/WritingClasses/SimpleClasses/SimpleClasses/Transaction.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class Transaction
+{
+private: 
+	int amount;
+	std::string type; //a better way exists
+
+public:
+	Transaction(int amt, std::string kind);
+	std::string Report();
+};
