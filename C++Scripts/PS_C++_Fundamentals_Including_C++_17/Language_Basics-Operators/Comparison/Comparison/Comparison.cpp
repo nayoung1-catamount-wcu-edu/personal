fe46db67feec64f4f2a99008ea5e3cf370889d80
nathayoung <nathanayoung@msn.com>
2020-11-28 23:05:23 -0500

    adding Pluralsight lessons

--- C++Scripts/PS_C++_Fundamentals_Including_C++_17/Language_Basics-Operators/Comparison/Comparison/Comparison.cpp
+++ C++Scripts/PS_C++_Fundamentals_Including_C++_17/Language_Basics-Operators/Comparison/Comparison/Comparison.cpp
@@ -3,13 +3,14 @@
 
 #include <iostream>
 using std::cout;
+using std::cin;
 using std::endl;
 
 #include "Utility.h"
 
 int main()
 {
-    int 1 = 2;
+    int i = 2;
     if (i == 3)
         cout << "i is 3" << endl;
     cout << " i " << i << endl;
@@ -17,6 +18,33 @@ int main()
         cout << "i is 3" << endl;
     cout << "i " << i << endl;
     i = 4;
-    if (3==i)
+    if (3 == i)
+        cout << "i is 3" << endl;
+    cout << "i " << i << endl;
+
+    //int p = foo("whatever");
+    //if (p != 0)
+    //    something();
+    //if (p = foo("something else"))
+    //    something();
+
+    cout << "enter a number " << endl;
+    cin >> i;
+
+    cout << i << " is";
+    if (!IsPrime(i))
+        cout << "not ";
+    cout << "prime." << endl;
+
+    int j;
+    cout << "enter a second number " << endl;
+    cin >> j;
+
+    cout << i << " is "; 
+    if (!(i % j == 0))
+        cout << "not ";
+    cout << "a multiple of " << j << endl;
+
+    return 0;
 
 }
