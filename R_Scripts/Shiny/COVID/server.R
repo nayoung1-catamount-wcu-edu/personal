9786ac39917a58e29549a2899593b328d6375c9b
nathayoung <nathanayoung@msn.com>
2020-12-05 13:20:55 -0500

    updates

--- R_Scripts/Shiny/COVID/server.R
+++ R_Scripts/Shiny/COVID/server.R
@@ -1,29 +1,22 @@
 library(reshape2)
 library(ggplot2)
 library(tidyverse)
-library(ggdark)
 
 # Define server logic
 server <- function(input, output, session) {
-  # read data
+# read data
   covid <-
     read.csv(file.path("Data/covid-19-data", "us-counties.csv"))
-  
-  # melt data
+# melt data
   covid_melt <-
-    melt(covid, id = c("date", "count
-y", "state", "fips"))
-  
-  # order county values ascending
+    melt(covid, id = c("date", "county", "state", "fips"))
+# order county values ascending
   covid_melt <- covid_melt[order(covid_melt$county), ]
-  
-  # order state values ascending
+# order state values ascending
   covid_melt <- covid_melt[order(covid_melt$state), ]
-  
-  # convert date column to date dtype
+# convert date column to date dtype
   covid_melt$date <- as.Date(covid_melt$date, format = "%Y-%m-%d")
-  
-  # output state_selector
+# output state_selector
   output$state_selector <- renderUI({
     selectInput(
       inputId = "state",
@@ -32,11 +25,9 @@ y", "state", "fips"))
       selected = ""
     )
   })
-  
-  # output county_selector
+# output county_selector
   output$county_selector <- renderUI({
     available <- covid_melt[covid_melt$state == input$state, "county"]
-    
     selectInput(
       inputId = "county",
       label = "Choose a County/Municipality:",
@@ -44,22 +35,19 @@ y", "state", "fips"))
       selected = unique(available[1])
     )
   })
-  
-  # output range of dates
+# output range of dates
   output$date_selector <- renderUI({
     dateRangeInput(inputId = "dates",
                    label = "Select a Range of Dates",
                    start = "2020-01-01")
   })
-  
-  # plot cases and deaths by selected state and county in selected state
+# plot cases and deaths by selected state and county in selected state
   output$covid_ggplot <- renderPlot({
     county_filter <- filter(covid_melt, county == input$county)
     ggplot(data = county_filter, aes(x = date,
                                      y = value,
                                      fill = variable)) +
       geom_bar(stat = "identity") +
-      scale_x_date(limits = c(input$dates[1], input$dates[2])) +
-      dark_theme_minimal()
+      scale_x_date(limits = c(input$dates[1], input$dates[2]))
   })
 }
