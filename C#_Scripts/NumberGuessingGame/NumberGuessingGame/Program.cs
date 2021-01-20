c011cda5ca0e462cc3ec35d7d0aef2040db7a143
nathayoung <nathanayoung@msn.com>
2021-01-19 22:06:42 -0500

    building C# projects

---
+++ C#_Scripts/NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number in between 0 and 100: ");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too high!  Guess a lower number...");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Too low!  Guess a higher number...");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("You win!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+
+            } while (win == false);
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
