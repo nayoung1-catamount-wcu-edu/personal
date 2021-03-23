7dcfcc76b4011b23f792999d6592525342a35602
nathayoung <nathanayoung@msn.com>
2021-03-23 18:37:36 -0400

    Updating COVID data for R Shiny App - Tue 03/23/2021

--- R/Shiny/DataDashboard/server.R
+++ R/Shiny/DataDashboard/server.R
@@ -1,10 +1,10 @@
 library(shiny)
+library(odbc)
 
 function(input, output, session) {
-  output$hist <- renderPlot({
-    hist(rnorm(input$num))
-  })
-  output$stats <- renderPrint({
-    summary(rnorm(input$num))
-  })
+  data <- dbConnect(odbc(),
+                    Driver = "SQL Server Native Client 11.0",
+                    Server = "localhost",
+                    Database = "DataDashboard",
+                    Trusted_Connection = "Yes")
 }
