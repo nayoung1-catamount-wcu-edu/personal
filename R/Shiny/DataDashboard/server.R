edb0dd09899ab4d62d0f522bfc4013fbde1b8234
nathayoung <nathanayoung@msn.com>
2021-03-24 06:21:43 -0400

    Updating COVID data for R Shiny App - Wed 03/24/2021

--- R/Shiny/DataDashboard/server.R
+++ R/Shiny/DataDashboard/server.R
@@ -1,10 +1,24 @@
 library(shiny)
 library(odbc)
 
-function(input, output, session) {
+server <- function(input, output, session) {
+  # read data from database
   data <- dbConnect(odbc(),
                     Driver = "SQL Server Native Client 11.0",
                     Server = "localhost",
                     Database = "DataDashboard",
                     Trusted_Connection = "Yes")
+
+  # split
+  data
+
+  output$state_selector <- renderUI({
+    selectInput(
+      inputId = "state",
+      label = "Choose a State:"
+      choices = unique(data)
+    )
+  })
 }
+
+
--- R/Shiny/DataDashboard/ui.R
+++ R/Shiny/DataDashboard/ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinythemes)
 
 fluidPage(
     theme = shinytheme(theme = "flatly"),
